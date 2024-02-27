42dc5d1533723c0b254ebb39f086eeed2649ab02
Soulbolt <soulbolt@gmail.com>
2024-02-27 14:47:55 -0700

    Add .env file to .gitignore. Add annotations to define table,id, and columns.

--- src/main/java/com/code/ecommerce/entity/Product.java
+++ src/main/java/com/code/ecommerce/entity/Product.java
@@ -1,8 +1,9 @@
 package com.code.ecommerce.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
 import java.util.Date;
@@ -11,15 +12,28 @@ import java.util.Date;
 @Table(name="product")
 @Data
 public class Product {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+    @Column(name = "sku")
     private String sku;
+    @Column(name = "name")
     private String name;
+    @Column(name = "description")
     private String description;
+    @Column(name = "unitPrice")
     private BigDecimal unitPrice;
+    @Column(name = "imageUrl")
     private String imageUrl;
+    @Column(name = "active")
     private boolean active;
+    @Column(name = "unitsInStock")
     private int unitsInStock;
+    @Column(name = "dateCreated")
+    @CreationTimestamp
     private Date dateCreated;
+    @Column(name = "lastUpdated")
+    @UpdateTimestamp
     private Date lastUpdated;
 }
