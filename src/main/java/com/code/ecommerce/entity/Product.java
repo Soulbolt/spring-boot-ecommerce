e8ad4c921d5f110314713d0870c77d499d54f874
Soulbolt <soulbolt@gmail.com>
2024-02-27 14:57:11 -0700

    Add new file ProductCategory, set up annotations to define table and properties. Define one to many relationship to Product and Many to one from Product to ProductCategory joincolumn by category_id.,

--- src/main/java/com/code/ecommerce/entity/Product.java
+++ src/main/java/com/code/ecommerce/entity/Product.java
@@ -16,23 +16,36 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private ProductCategory category;
+
     @Column(name = "sku")
     private String sku;
+
     @Column(name = "name")
     private String name;
+
     @Column(name = "description")
     private String description;
+
     @Column(name = "unitPrice")
     private BigDecimal unitPrice;
+
     @Column(name = "imageUrl")
     private String imageUrl;
+
     @Column(name = "active")
     private boolean active;
+
     @Column(name = "unitsInStock")
     private int unitsInStock;
+
     @Column(name = "dateCreated")
     @CreationTimestamp
     private Date dateCreated;
+
     @Column(name = "lastUpdated")
     @UpdateTimestamp
     private Date lastUpdated;
