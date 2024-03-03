6b86d59ee49961139f2bd1e6aa474fca0b3e6e7c
Soulbolt <soulbolt@gmail.com>
2024-03-03 13:04:56 -0700

    Add StateRepository interface. Annotate with CrossOrigin and RepositoryRestResource to expose states path. extends JpsRepositiry<State, Integer> and query list of states findByCountryCode @RequestParam code of type string.
    Refactor disable HTTP methods to use one disableHttpMethods and use it to pass Product, ProductCategory, Country, and State classes.

--- src/main/java/com/code/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/code/ecommerce/config/MyDataRestConfig.java
@@ -1,12 +1,15 @@
 package com.code.ecommerce.config;
 
+import com.code.ecommerce.entity.Country;
 import com.code.ecommerce.entity.Product;
 import com.code.ecommerce.entity.ProductCategory;
+import com.code.ecommerce.entity.State;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.metamodel.EntityType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.core.mapping.ExposureConfigurer;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -18,30 +21,30 @@ import java.util.Set;
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
 
-    private EntityManager entityManager;
+    private final EntityManager entityManager;
     @Autowired
     public MyDataRestConfig(EntityManager theEntityManager) {
         entityManager = theEntityManager;
     }
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
-//        RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
+//        RepositoryRestConfigure.super.configureRepositoryRestConfiguration(config, cors);
         HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
 
         //disable HTTP methods for Products: PUT, POST, DELETE
-        config.getExposureConfiguration()
-                .forDomainType(Product.class)
-                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-                        .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
-
-        //disable HTTP methods for ProductCategory: PUT, POST, DELETE
-        config.getExposureConfiguration()
-                .forDomainType(ProductCategory.class)
-                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-                        .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+        disableHttpMethods(config.getExposureConfiguration().forDomainType(Product.class), theUnsupportedActions);
+        disableHttpMethods(config.getExposureConfiguration().forDomainType(ProductCategory.class), theUnsupportedActions);
+        disableHttpMethods(config.getExposureConfiguration().forDomainType(Country.class), theUnsupportedActions);
+        disableHttpMethods(config.getExposureConfiguration().forDomainType(State.class), theUnsupportedActions);
 
         // Call an internal helper method
         exposeIds(config);
+    }
+
+    private static void disableHttpMethods(ExposureConfigurer config, HttpMethod[] theUnsupportedActions) {
+        config
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
     }
 
     private void exposeIds(RepositoryRestConfiguration config) {
