776e8a64f20612d6a84c84d532c5460c9bf81f35
Soulbolt <soulbolt@gmail.com>
2024-03-04 16:00:29 -0700

    Add service package, add CheckoutController, Annotate Crossorigin, RestController, RequestMapping. declare CheckoutService, call it. Add PostMapping annotation. to query purchase to save into data base.

---
+++ src/main/java/com/code/ecommerce/controller/CheckoutController.java
@@ -0,0 +1,25 @@
+package com.code.ecommerce.controller;
+
+import com.code.ecommerce.dto.Purchase;
+import com.code.ecommerce.dto.PurchaseResponse;
+import com.code.ecommerce.service.CheckoutService;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:4200")
+@RestController
+@RequestMapping("/api/checkout")
+public class CheckoutController {
+
+    private final CheckoutService checkoutService;
+
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+
+        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+        return purchaseResponse;
+    }
+}
