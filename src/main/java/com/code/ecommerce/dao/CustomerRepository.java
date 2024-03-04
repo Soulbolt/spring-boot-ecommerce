20947b44c14b17dfd13daf62ee780e987e94b513
Soulbolt <soulbolt@gmail.com>
2024-03-04 14:04:39 -0700

    Add new package dto, add Purchase and PurchaseRepository with their respective attributes and annotate with @Data. Add CustomerRepository Interface.

---
+++ src/main/java/com/code/ecommerce/dao/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.code.ecommerce.dao;
+
+import com.code.ecommerce.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
