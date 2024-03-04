acb90bd7b527d47b68dab657c4594acde0a37931
Soulbolt <soulbolt@gmail.com>
2024-03-04 12:04:35 -0700

    Add Customer and Address class entities, declared variables and annotate entity, table, getter, setter for class and id, generatedValue, column to each variable respectively.

---
+++ src/main/java/com/code/ecommerce/entity/Address.java
@@ -0,0 +1,32 @@
+package com.code.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "address")
+@Getter
+@Setter
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public Long id;
+
+    @Column(name = "street")
+    public String street;
+
+    @Column(name = "city")
+    public String city;
+
+    @Column(name = "state")
+    public String state;
+
+    @Column(name = "country")
+    public String country;
+
+    @Column(name = "zip_code")
+    public String zipCode;
+}
---
+++ src/main/java/com/code/ecommerce/entity/Customer.java
@@ -0,0 +1,26 @@
+package com.code.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "customer")
+@Getter
+@Setter
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public Long id;
+
+    @Column(name = "first_name")
+    public String firstName;
+
+    @Column(name = "last_name")
+    public String lastName;
+
+    @Column(name = "email")
+    public String email;
+}
