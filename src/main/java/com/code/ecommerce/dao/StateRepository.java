fa8729412c40a9a9694e5456a5dc2600072c9fc7
Soulbolt <soulbolt@gmail.com>
2024-03-07 09:18:43 -0700

    Remove unnecessary imports.

--- src/main/java/com/code/ecommerce/dao/StateRepository.java
+++ src/main/java/com/code/ecommerce/dao/StateRepository.java
@@ -3,7 +3,6 @@ package com.code.ecommerce.dao;
 import com.code.ecommerce.entity.State;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
