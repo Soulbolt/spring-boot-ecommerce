58d0ca280b3f943ce13af4097f89288696ab5b57
Soulbolt <soulbolt@gmail.com>
2024-03-07 13:43:41 -0700

    update find customer by email query with sorting desc. Update value for base path.

--- src/main/java/com/code/ecommerce/dao/OrderRepository.java
+++ src/main/java/com/code/ecommerce/dao/OrderRepository.java
@@ -4,9 +4,11 @@ import com.code.ecommerce.entity.Order;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@RepositoryRestResource
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-    Page<Order> findByCustomerEmail(@RequestParam("email") String email, Pageable pageable); // <>
+    Page<Order> findByCustomerEmailOrderByDateCreatedDesc(@RequestParam("email") String email, Pageable pageable);
 }
