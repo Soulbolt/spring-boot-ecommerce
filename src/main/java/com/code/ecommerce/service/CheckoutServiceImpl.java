88cccac06547a1794240a293568eef66e3c9fed7
Soulbolt <soulbolt@gmail.com>
2024-03-08 11:12:39 -0700

    Add PaymentInfo declare vars, implement PaymentIntent createPaymentIntent.

--- src/main/java/com/code/ecommerce/service/CheckoutServiceImpl.java
+++ src/main/java/com/code/ecommerce/service/CheckoutServiceImpl.java
@@ -1,24 +1,30 @@
 package com.code.ecommerce.service;
 
 import com.code.ecommerce.dao.CustomerRepository;
+import com.code.ecommerce.dto.PaymentInfo;
 import com.code.ecommerce.dto.Purchase;
 import com.code.ecommerce.dto.PurchaseResponse;
 import com.code.ecommerce.entity.Customer;
 import com.code.ecommerce.entity.Order;
 import com.code.ecommerce.entity.OrderItem;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 public class CheckoutServiceImpl implements CheckoutService {
 
-    private CustomerRepository customerRepository;
+    private final CustomerRepository customerRepository;
 
-     public CheckoutServiceImpl(CustomerRepository customerRepository){
+     public CheckoutServiceImpl(CustomerRepository customerRepository, @Value("${stripe.key.secret}") String secretKey) {
          this.customerRepository = customerRepository;
+         // Initialize Stripe API key
+         Stripe.apiKey = secretKey;
      }
     @Override
     @Transactional
@@ -59,6 +65,19 @@ public class CheckoutServiceImpl implements CheckoutService {
 
         // Return a response
         return new PurchaseResponse(orderTrackingNumber);
+    }
+
+    @Override
+    public PaymentIntent createPaymentIntent(PaymentInfo paymentInfo) throws StripeException {
+        List<String> paymentMethodTypes = new ArrayList<>();
+        paymentMethodTypes.add("card");
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("amount", paymentInfo.getAmount());
+        params.put("currency", paymentInfo.getCurrency());
+        params.put("payment_method_types", paymentMethodTypes);
+
+        return PaymentIntent.create(params);
     }
 
     private String generateOrderTrackingNumber() {
