580c7cf1f5664c0b788c4fb9379867eaba19294f
Soulbolt <soulbolt@gmail.com>
2024-03-08 11:29:18 -0700

    Add @PostMapping createPaymentIntent return paymentIntent json and http status.

--- src/main/java/com/code/ecommerce/controller/CheckoutController.java
+++ src/main/java/com/code/ecommerce/controller/CheckoutController.java
@@ -1,8 +1,13 @@
 package com.code.ecommerce.controller;
 
+import com.code.ecommerce.dto.PaymentInfo;
 import com.code.ecommerce.dto.Purchase;
 import com.code.ecommerce.dto.PurchaseResponse;
 import com.code.ecommerce.service.CheckoutService;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,5 +24,14 @@ public class CheckoutController {
     public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
 
         return checkoutService.placeOrder(purchase);
+    }
+
+    @PostMapping("/payment-intent")
+    public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInfo paymentInfo) throws StripeException {
+
+        PaymentIntent paymentIntent = checkoutService.createPaymentIntent(paymentInfo);
+        String paymentStr = paymentIntent.toJson();
+
+        return new ResponseEntity<>(paymentStr, HttpStatus.OK);
     }
 }
